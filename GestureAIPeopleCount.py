c9e3d795beae2871e5b9351e3f1bcad32b134e2a
Ayadetes <saiom12patro@gmail.com>
2026-03-28 10:58:09 -0400

    Add files via upload

--- GestureAIPeopleCount.py
+++ GestureAIPeopleCount.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
@@ -14,7 +15,13 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
     cv2.putText(frame, f'People Count: {len(faces)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.imshow("Video", frame)
+    matrix = np.ones(frame.shape, dtype="uint8")*50
+    step1 = cv2.add(frame, matrix)
+    (w,h) = step1.shape[:2]
+    center = (w//2,h//2)
+    M = cv2.getRotationMatrix2D(center,45,1)
+    step2 = cv2.warpAffine(step1, M, (w,h))
+    cv2.imshow("Video", step2)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
